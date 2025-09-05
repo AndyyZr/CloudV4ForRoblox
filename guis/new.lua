85da722169ca9b3a3557941bc74c4062e0267253
Andyy <yopieamg@gmail.com>
2025-09-04 18:41:36 -0700

    Update new.lua

--- guis/new.lua
+++ guis/new.lua
@@ -5793,7 +5793,7 @@ mainapi:CreateCategory({
 	Size = UDim2.fromOffset(19, 12)
 })
 mainapi:CreateCategory({
-	Name = 'AcornWare',
+	Name = 'Cloud',
 	Size = UDim2.fromOffset(14, 14)
 })
 mainapi.Categories.Main:CreateDivider('misc')
@@ -6087,7 +6087,7 @@ guipane:CreateButton({
 			MinigamesCategory = 8,
 			FriendsCategory = 9,
 			ProfilesCategory = 10,
-			AcornWareCategory = 11
+			CloudCategory = 11
 		}
 		local categories = {}
 		for _, v in mainapi.Categories do
