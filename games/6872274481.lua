ea0e904b01c162b24fd3c65521c3bbb7b8fd067c
Andyy <yopieamg@gmail.com>
2025-09-04 17:49:42 -0700

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -9697,7 +9697,7 @@ end)
 
 if not isfile('newvape/profiles/nofirst.txt') then
 	writefile('newvape/profiles/nofirst.txt', 'true')
-	notif('Cloud', 'nuker == breaker', 4.5)
+	notif('AndyV2', 'nuker == breaker', 4.5)
 end
 
 run(function()
@@ -9806,4 +9806,4 @@ run(function()
     })
 end)
 																																						
-notif('Cloud', 'Loaded successfully!', 10)
+notif('AndyV2', 'Loaded successfully!', 10)
